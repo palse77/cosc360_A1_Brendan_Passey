d889f6576e3aa0de920c891dc2514d3073a0c87a
brendanpassey <brendanpassey@gmail.com>
2024-09-19 18:49:02 +1000

    post API set up

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/posts', [PostController::class, 'index']); //data?
