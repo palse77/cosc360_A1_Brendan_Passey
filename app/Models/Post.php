940702d8ca9912ce07b10b33db468ace8037abf5
brendanpassey <brendanpassey@gmail.com>
2024-09-19 18:13:27 +1000

    Installed_API Notifiable, HasAPITokens in user and post models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use MongoDB\Laravel\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $fillable = ['title', 'content', 'user_id'];
 
